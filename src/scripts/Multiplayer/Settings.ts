c26f461ed49e18186101b4c3f11653e6106fa96d
NateRiz <nathan.rizik@gmail.com>
2022-05-18 19:31:44 -0700

    diff settings and rule persist over network not impl

--- src/scripts/Multiplayer/Settings.ts
+++ src/scripts/Multiplayer/Settings.ts
@@ -1,10 +1,21 @@
 export default class Settings{
     onSettingsChange: ()=>void;
-    lives: number = 1
+    lives: number = 1;
+    doesRulePersist: boolean = false;
+    difficulty: number = 50;
+
+    difficultyRange: HTMLInputElement;
+    difficultySpan: HTMLSpanElement;
+    rulePersists: HTMLInputElement;
+
     static noop = ()=>{};
 
     constructor(onSettingsChange: ()=>void = Settings.noop){
         this.onSettingsChange = onSettingsChange;
+        this.difficultyRange = document.querySelector("#DifficultyRange") as HTMLInputElement;
+        this.rulePersists = document.querySelector("#RulePersists") as HTMLInputElement;
+        this.difficultySpan = document.querySelector("#DifficultyNumber") as HTMLSpanElement;
+
         if(this.onSettingsChange === Settings.noop){
             return; // client
         }
@@ -22,19 +33,51 @@ export default class Settings{
         ThreeLifeButton.onclick = () => {
             this.SetLives(3)
         }
+
+        this.difficultyRange.disabled = false;
+        this.difficultyRange.oninput = () => {
+            this.difficultySpan.textContent = this.difficultyRange.value;
+        }
+        this.difficultyRange.onchange = () => {
+            this.difficulty = parseInt(this.difficultyRange.value);
+            onSettingsChange();
+        }
+
+        this.rulePersists.disabled = false;
+        this.rulePersists.onchange = () => {
+            this.doesRulePersist = this.rulePersists.checked;
+            onSettingsChange();
+        }
     }
 
     public static CreateSettingsFromJSON(json: any){
         const settings = new Settings();
         Object.assign(settings, json);
+
         settings.SetLives(settings.lives);
+        settings.SetDifficulty(settings.difficulty);
+        settings.SetDoesRulePersist(settings.doesRulePersist)
+
         return settings;
     }
 
     toJSON(){
         return {
-            lives: this.lives
+            lives: this.lives,
+            difficulty: this.difficulty,
+            doesRulePersist: this.doesRulePersist,
         };
+    }
+
+    public SetDifficulty(difficulty: number){
+        this.difficulty = difficulty;
+        this.difficultySpan.textContent = difficulty.toString();
+        this.difficultyRange.value = difficulty.toString();
+    }
+
+    public SetDoesRulePersist(doesRulePersist: boolean){
+        this.doesRulePersist = doesRulePersist;
+        this.rulePersists.checked = doesRulePersist;
     }
 
     public SetLives(lives: number){
